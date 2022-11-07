0b52479423c8cc4edfaf196d6e8120147e81e129
DevX4 <71060210+DevX4@users.noreply.github.com>
2022-11-07 22:57:08 +0530

    Add files via upload

---
+++ BalacingOfBrackets.java
@@ -0,0 +1,54 @@
+package lab3.DSA.Ques1;
+
+import java.util.Stack;
+
+public class BalacingOfBrackets {
+
+	public static void main(String[] args) {
+		//hardcoded input
+		//Enter your input here !
+		String input = "([[{}]])";
+		boolean result = isBalancedBracketsExp(input);
+		if(result) {
+			System.out.println("The entered String is balanced.");
+		}else {
+			System.out.println("The entered string is not balanced.");
+		}
+
+	}
+	
+	public static boolean isBalancedBracketsExp(String input) {
+		
+		Stack<Character> data = new Stack<Character>();
+		for(int i=0; i<input.length();i++) {
+			char ch = input.charAt(i);
+			if(ch == '(' || ch == '[' || ch == '{') {
+				data.push(ch);
+				continue;
+			}
+			if(data.isEmpty()) {
+				return false;
+			}
+			char matchCh = data.pop();
+			switch(ch) {
+			case ')' :
+				if(matchCh != '(') {
+					return false;
+				}
+				break;
+			case ']' :
+				if(matchCh != '[') {
+					return false;
+				}
+				break;
+			case '}' :
+				if(matchCh != '{') {
+					return false;
+				}
+				break;
+			}
+		}
+		return data.isEmpty();
+	}
+
+}
